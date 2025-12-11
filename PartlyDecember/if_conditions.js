31d0c9c713e87a0de294097a5b1945bcf0cda8f2
Blaben <kutoh.bhcvoh@gmail.com>
2025-12-11 17:12:23 +0000

    else if condition sample 1

---
+++ PartlyDecember/if_conditions.js
@@ -0,0 +1,14 @@
+//Using the if Else condition
+
+/*
+If time is less than 10:00, create a "Good morning" greeting, if not, but time is less than 20:00, create a "Good day" greeting, otherwise a "Good evening":
+*/
+let time = 8;
+
+if (time < 10) {
+  console.log("Good morning");
+} else if (time < 20) {
+  console.log("Good day");
+} else {
+  console.log("Good evening");
+}
