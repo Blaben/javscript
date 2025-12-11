6f3d6d89bca01c8f9669f302f1f7740ec991c291
Blaben <kutoh.bhcvoh@gmail.com>
2025-12-11 15:22:55 +0000

    my javascript codes

---
+++ index.js
@@ -0,0 +1,18 @@
+let firstname = "Hello";
+let lastname = " Blaben";
+console.log(firstname + lastname);
+//james is a man
+
+/* James is a game boy
+Yes it was just a comment*/
+//Dictioneries
+
+const userlog1 = {
+  name: "kutoh.bhcvoh@gmail.com",
+  password: "RuDiGA2310",
+};
+
+if ((userlog1.name = "kutoh.bhcvoh@gmail.com" && userlog1.password== "RuDiGA2310"))
+  console.log("Logged in success");
+else console.log("Wrong details recorded");
+console.log("End of line");
