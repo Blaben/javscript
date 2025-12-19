1fe4e6472be6f7c4cceedd3185c9f009592ed989
Blaben <kutoh.bhcvoh@gmail.com>
2025-12-19 12:28:12 +0000

    small notes on for and while loop

---
+++ loops/loop1.js
@@ -0,0 +1,25 @@
+// let Age = [20, 18, 20, 25, 22, 28];
+
+// for (let i = 0; i < Age.length; i++) {
+//   console.log(Age[i]);
+// }
+
+// Use a for loop to print numbers from 10 to 1.
+
+for (let x = 10; x > 0; x--) {
+  console.log(x);
+}
+
+//use a while loop to print even numbers from 2 to 20
+
+let y = 2;
+while (y <= 20) {
+  console.log(y);
+  y += 2;
+}
+
+// using a for loop to print even numbers from 2 to 20
+
+// for (z = 2; z <= 20; z += 2) {
+//   console.log(z);
+// }
